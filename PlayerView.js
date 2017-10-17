84b589f1f8e82d51ceaf5a7cc09a4a8320a3580b
Dylan Gyesbreghs <dgyesbreghs@gmail.com>
2017-10-17 20:12:24 +0200

    Modify PlayerView

--- PlayerView.js
+++ PlayerView.js
@@ -1,4 +1,2 @@
 import { requireNativeComponent } from 'react-native';
-
-// requireNativeComponent automatically resolves 'RCTPlayerView' to 'RCTPlayerViewManager'
 module.exports = requireNativeComponent('RCTPlayerView', null);
